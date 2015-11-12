0af02530354469188fc98dc796a048f1ea9cbbc1
AlexHikari <alriv01@ucm.es>
2015-11-12 16:33:01 +0100

    GISA2b ok
    Pasa el juez

---
+++ GISA2b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int N = 100000;
+
+
+int main(){
+
+	int casos, numelems, v[N], cont = 0, min, resultado[N];
+	cin >> casos;
+	int casosaux = casos;
+	while (casos > 0){
+
+		cin >> numelems;
+		for (int i = 0; i < numelems; i++){
+			cin >> v[i];
+		}
+		min = v[numelems-1];
+		for (int j = numelems - 2; j >= 0; j--){
+			if (v[j] > min) cont++;
+			else if (v[j] < min) min = v[j];
+		}
+		resultado[casos] = cont;
+		cont = 0;
+		casos--;
+	}
+
+	for (int i = casosaux; i > 0; i--){
+		cout << resultado[i] << endl;
+	}
+	return 0;
+}
